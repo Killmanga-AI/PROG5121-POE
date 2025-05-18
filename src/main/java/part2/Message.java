f2035dbfd589ae05ea8edb7f8e6cf2416b215394
Killmanga-AI <abdullatifrajabu7@.com>
2025-05-18 19:39:38 +0200

    Structured repo

---
+++ src/main/java/part2/Message.java
@@ -0,0 +1,15 @@
+package part2;
+
+public class Message {
+    String messageID;
+    int messagesNumber;
+    String recipient;
+    String messageText;
+    String messageHash;
+    boolean isSent;
+    boolean isStored;
+    boolean isDisregarded;
+
+
+
+}
---
+++ src/test/java/part1/LoginTest.java
@@ -0,0 +1,40 @@
+package part1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginTest {
+
+    @Test
+    void testValidUsername() {
+        assertTrue(Login.isValidUsernameEnhanced("ben_1"));
+    }
+
+    @Test
+    void testInvalidUsername() {
+        assertFalse(Login.isValidUsernameEnhanced("invalid_user"));
+    }
+
+    @Test
+
+    void testValidCellphone() {
+        assertTrue(Login.isValidCellphoneNumberInternational("+27812345678"));
+    }
+
+    @Test
+    void testInvalidCellphone() {
+        assertFalse(Login.isValidCellphoneNumberInternational("12345"));
+    }
+
+    @Test
+    void testStrongPassword() {
+        assertTrue(Login.isStrongPasswordEnhanced("Pass123!"));
+    }
+
+    @Test
+    void testWeakPassword() {
+        assertFalse(Login.isStrongPasswordEnhanced("weakness"));
+    }
+}
+
